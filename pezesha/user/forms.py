2c1b0ded9138fb3e55b38e462da41a8d4e31338b
peshy <wambowmagola7@gmail.com>
2019-06-26 15:41:11 +0300

    Error Debugged

---
+++ pezesha/user/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields="__all__"
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=("full_names","email","password", "confirm_password")
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields="__all__"
